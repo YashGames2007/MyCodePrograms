7d6d9e621ed65f4d56b080fc296435bfce64ebf7
YashGames2007 <manojbhavsar1973@gmail.com>
2022-05-20 10:27:36 +0530

    Web Development Journey Begins.

--- Python/Advanced Python Programming/Static Programming In Python/Algorithms/liner_Search.py
+++
@@ -1,6 +0,0 @@
-def linear_Search(_list, target):
-    for index in range(len(_list)):
-        if _list[index] == target:
-            return index
-    return None
-
--- Python/Advanced Python Programming/Static Programming In Python/Object Oriented Programming - OOPs/Operation_On_Equations.py
+++
@@ -1,7 +0,0 @@
-import Polinomials
-
-e1 = Polinomials.Equations("- 3x^2 + 4y - 5z + 24 = 12")
-print("\n\n\n")
-e1 = Polinomials.Equations("2x + 4y - 5z + 24 = 12")
-print("\n\n\n")
-e2 = Polinomials.Equations("x + y = 0")
