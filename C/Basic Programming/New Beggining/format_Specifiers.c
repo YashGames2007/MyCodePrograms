f5d7ba1b9dfca8cbbf9df6b49c0780d7224c76ed
YashGames2007 <manojbhavsar1973@gmail.com>
2022-05-25 19:22:37 +0530

    Added New Beginning - Format Specifiers.

---
+++ C/Basic Programming/New Beggining/format_Specifiers.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define pi 3.14159
+
+int main()
+{
+    // // /*
+    // // Data Types And There Format Specifiers:
+    // //     ~ Int - %d
+    // //     ~ Float - $f
+    // //     ~ Char - %c
+    // //     ~ Long - %l
+    // //     ~ Double
+    // // */
+
+    // // int a = 8;
+    // // float b = 7.3330000;
+    // // printf("\nThis Specifies Will Print Float: %f", b);
+    // // printf("\nThis Specifier Will Take Exactly 17 Places With Adding Padding Before Float: %17f", b);
+    // // printf("\nThis Specifier Will Set The Precision of Float To 3 Digits: %.3f", b);
+    // // printf("\nPI: %f", pi);
+    // // int i;
+    // // printf("\n");
+    // // scanf("%d", &i);
+    // // switch (i)
+    // // {
+    // // case 1:
+    // //     printf("\ni Is 1");
+    // //     break;
+    // // default:
+    // //     printf("\ni Is Unknown.");
+    // //     break;
+    // // }
+    // int num, i = 1;
+    // printf("\nType A Number: ");
+    // scanf("%d", &num);
+    // printf("\nThe Multiplication Table Of %d", num);
+
+    // printf("\n%d X %d  = %d", num, i, num*i);
+    // i++;
+    // printf("\n%d X %d  = %d", num, i, num*i);
+    // i++;
+    // printf("\n%d X %d  = %d", num, i, num*i);
+    // i++;
+    // printf("\n%d X %d  = %d", num, i, num*i);
+    // i++;
+    // printf("\n%d X %d  = %d", num, i, num*i);
+    // i++;
+    // printf("\n%d X %d  = %d", num, i, num*i);
+    // i++;
+    // printf("\n%d X %d  = %d", num, i, num*i);
+    // i++;
+    // printf("\n%d X %d  = %d", num, i, num*i);
+    // i++;
+    // printf("\n%d X %d  = %d", num, i, num*i);
+    // i++;
+    // printf("\n%d X %d = %d", num, i, num*i);
+    // i++;
+    
+    return 0;
+}
