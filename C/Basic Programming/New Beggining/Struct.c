53bdb406f60111ab49240f32ca35e9342a20b7cd
YashGames2007 <manojbhavsar1973@gmail.com>
2022-05-28 12:06:14 +0530

    Added MSBTE Unit III Infix to Postfix Con. - Rules(Modified), Example3, Example4.

--- C/Basic Programming/New Beggining/Struct.c
+++ C/Basic Programming/New Beggining/Struct.c
@@ -1,20 +1,51 @@
 #include <stdio.h>
 
-struct Student
-{
-    char name[50];
-    char major[50];
-    int age;
-    double gpa;
-};
-
+// int fib_Recursive(int n);
+// int fib_Iterative(int n);
 
 int main()
 {
-    struct Student stud1;
-    stud1.age = 22;
-    stud1.gpa = 3.2;
-    strcpy(stud1.name, "Rahul");
-    strcpy(stud1.major, "Business");
+    int a, *b, **c;
+    a = 1;
+    b = NULL;
+    c = &b;
+
+    int arr[3] = {a, *b, **c};
+    printf("\n%x, %x, %x", &a, &*b, &**c);
+    printf("\n%x, %p, %x", &arr[0], &arr[1], &arr[2]);
+    // printf("\nIterative: %d", fib_Iterative(45));
+    // printf("\nRecursive: %d", fib_Recursive(45));
     return 0;
 }
+
+// int fib_Recursive(int n)
+// {
+//     if (n <= 0)
+//     {
+//         return 0;
+//     }
+//     else if (n == 1 || n == 2)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         int value = fib_Recursive(n - 1) + fib_Recursive(n - 2);
+//         return value;
+//     }   
+// }
+
+// int fib_Iterative(int n)
+// {
+//     int prev1, prev2, current;
+//     prev2 = 0;
+//     prev1 = 1;
+//     current = 1;
+//     for (int i = 2; i < n; i++)
+//     {
+//         prev2 = prev1;
+//         prev1 = current;
+//         current = prev1 + prev2;
+//     }
+//     return current;
+// }
