3798a62e23981fcf7388e0a5ac3bcd7e0fb1e8f7
YashGames2007 <manojbhavsar1973@gmail.com>
2022-05-30 14:15:55 +0530

    Added Array_Out - Array Printer.

--- C/Basic Programming/New Beggining/newBeggining.c
+++ C/Basic Programming/New Beggining/newBeggining.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 
+struct arr
+{
+    int head;
+    int *base;
+    
+};
+
+
 int main()
 {
+    // char f[] = "yashgames2007";
+    char arr[10];
+    gets(arr);
+    puts(arr);
+    // printf("\n%s", f);
     // int a; scanf("%d", &a);
     // int i = a < 5 ?3 :4;
     // printf("\n%d", i);
     // printf("Type a Number: ");
     // scanf("%d", &i);
 
-    char arr[10] = {'c', 'a', 't', 'a', 'l', 'i', 's', 't', 'e', 'r'}, temp;
+    // char arr[10] = {'c', 'a', 't', 'a', 'l', 'i', 's', 't', 'e', 'r'}, temp;
 
-    int j = 1;
+    // int j = 1;
     // for (int i = 0; i < 10; i++)
     // {
     //     printf("\nType The Character For Array At Index %d : ", i);
     //     scanf("%c%c", &arr[i], &temp);
     // }
-    printf("\nYour Character Array: {");
+    // printf("\nYour Character Array: {");
 
-    for (int i = 0; i < 9; i++)
-    {
-        printf("'%c', ", arr[i]);
-    }
+    // for (int i = 0; i < 9; i++)
+    // {
+    //     printf("'%c', ", arr[i]);
+    // }
 
-    printf("'%c'}.", arr[9]);
+    // printf("'%c'}.", arr[9]);
     
     return 0;
 }
