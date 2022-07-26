1d3c4b610e15e873359ea3f45c77411f18da7e18
YashGames2007 <manojbhavsar1973@gmail.com>
2022-07-26 12:58:10 +0530

    Started Object Oriented Programming In C++.

--- C++/New Beginning/new.cpp
+++ C++/New Beginning/new.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+    return;
+}
+
+void swap(int &a, int &b)
+{
+    return;
+}
+
 int main()
 {
-    cout <<"\n"<<"Hello World!";
+    // 6! = 6*5*4*3*2*1
+    int a = 45;
+    int &b = a;
+    // scanf("%d", &a);
+    cout <<endl<<"Before Swap: "<<a<<", "<<b;
+    swap(a, b);
+    cout <<endl<<"After Swap: "<<a<<", "<<b;
     return 0;
 }
