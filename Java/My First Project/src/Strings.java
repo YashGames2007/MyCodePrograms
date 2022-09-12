64d142f41262a73e3b4d956d060e60444866fb27
YashGames2007 <manojbhavsar1973@gmail.com>
2022-09-12 21:15:27 +0530

    Coded First Java Program.

---
+++ Java/My First Project/src/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+        // Program About Strings in java.
+        String password = "YashGames2007";
+
+        String format_New = "Programs";
+        String format_Old = "Games";
+
+        System.out.println(password.replace(format_Old, format_New));
+        
+    }
+}
