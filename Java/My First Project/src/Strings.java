3630d4d31e873d6d66db29fcf5fbfaaa0e007824
YashGames2007 <manojbhavsar1973@gmail.com>
2022-09-14 22:13:25 +0530

    Added More Files.

--- Java/My First Project/src/Strings.java
+++ Java/My First Project/src/Strings.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
 public class Strings {
     public static void main(String[] args) {
         // Program About Strings in java.
-        String password = "YashGames2007";
+        String password;
 
-        String format_New = "Programs";
-        String format_Old = "Games";
+        // String format_New = "Programs";
+        // String format_Old = "Games";
 
-        System.out.println(password.replace(format_Old, format_New));
-        
+        // System.out.println(password.replace(format_Old, format_New));
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Enter The Password: ");
+        password = input.next();
+
+        switch (password) {
+            case "YashGames2007" -> {
+                System.out.printf("\nCorrect Password");
+            }
+            case "YashPrograms2007" -> {
+                System.out.printf("\nSecondary Password Accessed!");
+            }
+            default -> {
+                System.out.printf("\nIncorrect Password, Access Denied!!!");
+            }
+        }
+        input.close();
     }
 }
