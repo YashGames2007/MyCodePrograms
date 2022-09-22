774a6be4f07c958c8912a8ee6c128d8b069cfd7b
YashGames2007 <manojbhavsar1973@gmail.com>
2022-09-22 17:34:53 +0530

    Learning Curve.

---
+++ C/Basic Programming/basic_Structure.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int side;
+    printf("Enter the Side of the Square: ");
+    scanf("%d",&side);
+    printf("\nThe Area of Square is: %d", side*side);
+    return 0;
+}
