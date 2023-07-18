2f9cbf20b1abadf7ca322e60544e2606dead2eed
YashGames2007 <yashbhavsar.dev2007@gmail.com>
2023-07-18 19:17:50 +0530

    At lecture 40 - CWH Ultimate JS Course

--- .vscode/JavaScript/beginning.js
+++ .vscode/JavaScript/beginning.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+const prompt=require("prompt-sync")({sigint:true});
+
+let str = "Please give Rs 1000";
+let a = Number.parseInt(str.slice(str.indexOf("Rs ")+3));
+
+console.log(a);
