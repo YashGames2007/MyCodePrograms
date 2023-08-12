91d32c5474930de3b1dfa10bbbf281717e53df3e
YashGames2007 <yashbhavsar.dev2007@gmail.com>
2023-08-12 10:12:01 +0530

    Chess - V1.1 Added:     Project Distribution     OOPs implementation     Game Constants Setup     Game Objects Setup

--- Python/Pygame Basic to Advanced/Chess Game/Scripts/game_constants.py
+++ Python/Pygame Basic to Advanced/Chess Game/Scripts/game_constants.py
@@ -2,36 +2,51 @@ import os
 
 root_dir = "Python/Pygame Basic to Advanced/Chess Game/"
 
-object_size_ratio = 6.52174
+object_size_ratio = 6.52174  # Constant term to change the objects size
+colors = ["white", "black"]
+tokens = ["pawn", "rock", "knight", "bishop", "queen", "king"]
+layers = {"white": [7, 6], "black": [0, 1]}
 
-object_locations = {
-    "white pawn": os.path.join(root_dir, "Assets/white tokens/pawn.png"),
-    "white rock": os.path.join(root_dir, "Assets/white tokens/rock.png"),
-    "white knight": os.path.join(root_dir, "Assets/white tokens/knight.png"),
-    "white bishop": os.path.join(root_dir, "Assets/white tokens/bishop.png"),
-    "white queen": os.path.join(root_dir, "Assets/white tokens/queen.png"),
-    "white king": os.path.join(root_dir, "Assets/white tokens/king.png"),
-    "black pawn": os.path.join(root_dir, "Assets/black tokens/pawn.png"),
-    "black rock": os.path.join(root_dir, "Assets/black tokens/rock.png"),
-    "black knight": os.path.join(root_dir, "Assets/black tokens/knight.png"),
-    "black bishop": os.path.join(root_dir, "Assets/black tokens/bishop.png"),
-    "black queen": os.path.join(root_dir, "Assets/black tokens/queen.png"),
-    "black king": os.path.join(root_dir, "Assets/black tokens/king.png"),
-    "chess board": os.path.join(root_dir, "Assets/board.jpg")
-}
+
+object_locations = {"chess board": os.path.join(root_dir, "Assets/board.jpg")}
+# Coping Token Locations in the object_locations dict
+for color in colors:
+    for token in tokens:
+        object_locations[f"{color} {token}"] = os.path.join(
+            root_dir, f"Assets/{color} tokens/{token}.png"
+        )
 
 object_sizes = {
-    "white pawn": (9, 24.9),
-    "white rock": (9.1, 23.1),
-    "white knight": (9.7, 24.8),
-    "white bishop": (9.7, 22),
-    "white queen": (9.9, 19.4),
-    "white king": (9.7, 18.8),
-    "black pawn": (9, 24.9),
-    "black rock": (9.1, 23.1),
-    "black knight": (9.7, 24.8),
-    "black bishop": (9.7, 22),
-    "black queen": (9.9, 19.4),
-    "black king": (9.7, 18.8),
-    "chess board": (11.5, 11.5)
+    "pawn": (9, 24.9),
+    "rock": (9.1, 23.1),
+    "knight": (9.7, 24.8),
+    "bishop": (9.7, 22),
+    "queen": (9.9, 19.4),
+    "king": (9.7, 18.8),
+    "chess board": (115, 115),
 }
+
+tokens_ofset = {
+    "pawn": (12, -95),
+    "rock": (9, -85),
+    "knight": (10, -95),
+    "bishop": (10, -77),
+    "queen": (10, -60),
+    "king": (10, -56),
+}
+
+block_locations = [67.5, 151, 235.5, 317, 401.5, 484, 567.5, 651]
+board_positions = [[(x, y) for y in block_locations] for x in block_locations]
+
+# Creating Game Constant Variables...
+screen_Width = 800
+screen_Height = 800
+fps_Value = 1
+
+# Adding Some Colors...
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+background = (255, 191, 141)
--- Python/Pygame Basic to Advanced/Chess Game/Scripts/game_objects.py
+++ Python/Pygame Basic to Advanced/Chess Game/Scripts/game_objects.py
@@ -1,39 +1,75 @@
 import pygame
 import game_constants as const
 
+
+# Initializing Pygame Functions and Attributes...
+pygame.init()
+pygame.display.init()
+
+clock = pygame.time.Clock()
+game_Window = pygame.display.set_mode((const.screen_Width, const.screen_Height))
+pygame.display.set_caption("Chess - YashGames2007")
+
 def image(path:str, size:tuple, scale:float) -> pygame.Surface:
     return pygame.transform.scale(pygame.image.load(path).convert_alpha(), (size[0]*scale, size[1]*scale))
 
 def object_loader(name:str):
-    return image(const.object_locations[name], const.object_sizes[name], const.object_size_ratio)
+    return image(const.object_locations[name], const.object_sizes[name.replace("white ", "").replace("black ", "")], const.object_size_ratio)
+
+class Token:
+    game_tokens = dict()
+
+    def init() -> None:
+
+        for color in const.colors:
+            for token in const.tokens:
+                Token.game_tokens[f"{color} {token}"] = object_loader(f"{color} {token}")
 
 
-white_tokens = []
-black_tokens = []
+    # Used to render one token of the board
+    @classmethod
+    def render(cls, token_name:str, pos_x:int, pos_y:int) -> None:
+        position = const.board_positions[pos_x][pos_y]
+        ofset = const.tokens_ofset[token_name.replace("white ", "").replace("black ", "")]
+        game_Window.blit(Token.game_tokens[token_name], (position[0]+ofset[0], position[1]+ofset[1]))
+    
 
-for i in range(2):
-    if i == 0:
-        tokens = white_tokens
-        color = "white"
-    else:
-        tokens = black_tokens
-        color = "black"
+class Board:
 
+    board_padding = 50  #Making the board 50 pixel smaller than game_Window
+    board = object_loader("chess board")
 
-    for _ in range(8):
-        tokens.append(object_loader(f"{color} pawn"))
+    def __init__(self) -> None:
+        Token.init()
+        self.token_board = [[None for x in range(8)] for y in range(8)]
 
-    for _ in range(2):
-        tokens.append(object_loader(f"{color} rock"))
+    def move_token(self, token_name:str, pos_from:tuple, pos_to:tuple) -> None:
+        if self.token_board[pos_from[0]][pos_from[1]] == token_name:
+            self.token_board[pos_from[0]][pos_from[1]] = None
+            self.token_board[pos_to[0]][pos_to[1]] = token_name
 
-    for _ in range(2):
-        tokens.append(object_loader(f"{color} knight"))
+    def render_tokens(self):
+        # print(self.token_board)
+        for x in range(8):
+            for y in range(8):
+                token = self.token_board[x][y]
 
-    for _ in range(2):
-        tokens.append(object_loader(f"{color} bishop"))
+                if token != None:
+                    Token.render(token, x, y)
 
-    tokens.append(object_loader(f"{color} queen"))
-    tokens.append(object_loader(f"{color} king"))
+    def reset_board(self):
+        for color in const.colors:
+            layer = const.layers[color]
+            # Setting Pawns positions...
+            for x in range(8):
+                self.token_board[x][layer[1]] = f"{color} pawn"
 
-print(white_tokens)
-print(black_tokens)
+            # Setting Rocks, Knights, & Bishops positions...
+            for x, token in enumerate(["rock", "knight", "bishop"]):
+                self.token_board[x][layer[0]] = f"{color} {token}"
+                self.token_board[7-x][layer[0]] = f"{color} {token}"
+            
+            # Setting Queens & Kings positions...
+            self.token_board[3][layer[0]] = f"{color} queen"
+            self.token_board[4][layer[0]] = f"{color} king"
+        
