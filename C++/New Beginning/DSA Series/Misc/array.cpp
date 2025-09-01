136bc2c0c3b74a282bbf7a7bbca298b71be330d4
YashGames2007 <yashbhavsar.dev2007@gmail.com>
2025-09-01 23:11:24 +0530

    DSA Series: Day 4
    Lecture  9-12

---
+++ C++/New Beginning/DSA Series/Misc/array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[5] = {};
+
+    printArray(arr, 5);
+    return 0;
+}
